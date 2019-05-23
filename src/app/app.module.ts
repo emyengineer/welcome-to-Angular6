2da403529c1b01bb57c367fa897b3164c74a7a16
emyengineer <emyengineer@gmail.com>
2019-05-23 03:24:52 +0200

    7. Creating Components with the CLI & Nesting Components    ng generate component servers    ng g c servers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
