1ea850f65ff15b15ec7db376fab37f049bd7faa6
emyengineer <emyengineer@gmail.com>
2019-05-23 02:31:47 +0200

    4. Creating a New Component Component Selector: is the HTML tag that your are going to use inside you page

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server/server.component.html'
+})
+
+export class ServerComponent {
+
+}
