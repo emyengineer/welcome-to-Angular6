912b7b7ab863a59165e1e6e4ec58ee1389d562bd
emyengineer <emyengineer@gmail.com>
2019-05-24 03:05:07 +0200

    13. String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    
+    serverId: number = 10;
+    serverStatus: string = 'Offline';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
 
 }
